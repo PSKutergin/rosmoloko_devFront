3ee27eba29b2a6519965320f5af8f099804c7736
PSKutergin <PSKutergin@gmail.com>
2023-12-19 17:31:06 +0300

    add page about and it`s adaptive

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+import { resolve } from 'path'
+
 export default {
-    base: '/rosmoloko_devFront/'
+    base: '/rosmoloko_devFront/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about/index.html'),
+            }
+        }
+    }
 }
